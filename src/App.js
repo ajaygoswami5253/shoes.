6f25b3b2cfe59f0cfee279096be4b3897b6cc087
ajaygoswami5253 <151458981+ajaygoswami5253@users.noreply.github.com>
2024-03-11 07:01:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './componenets/Header';
+ import { Ramu } from './componenets/Ramu';
+function App() {
+  return (
+    <div>
+    <Header/>
+     <Ramu/>
+    </div>
+  );
+}
+
+export default App;
